93f8e00bb07115d639b49d0df18749496b8beaa8
dinware <dinware10@gmail.com>
2019-01-02 04:12:39 +0800

    update

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                         <div class="col-sm-6 col-sm-offset-3 form-box">
                         	<div class="form-top">
                         		<div class="form-top-left">
-                        			<h3>Login to POS system</h3>
+                        			<h3>Login to POS systems</h3>
                             		<p>Enter your username and password to login</p>
                         		</div>
                         		<div class="form-top-right">
